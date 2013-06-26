4140d2e12c92af34199c57f22454e616b66100b7
Fatih Soydan <fatihsoydan@fatihsoydan.com>
2013-06-26 18:48:16 +0300

    checkUser added

--- test/testme.js
+++ test/testme.js
@@ -1,4 +1,7 @@
-var ldap2json = require('../lib/ldap2json');
+var 
+	ldap2json = require('../lib/ldap2json')
+	,vows 		= require('vows')
+	;
 
 
 var getTreeOptions = {
@@ -8,12 +11,35 @@ var getTreeOptions = {
   base : 'dc=bh,dc=pvt' 
 }
 
+
+/// FIND A PUBLIC LDAP Server for testing	
+
+
+ldap2json.checkUser(getTreeOptions,function(e){
+	if (e){
+		console.log("Error : : : : "+e);
+	} else {
+		console.log("Authentication OK");  
+	}
+});
+
+
 ldap2json.getJson(getTreeOptions,function(e,tree){
 	if (e){
 		console.log("Error : : : : "+e);
-		process.exit(0); /// FIND A PUBLIC LDAP Server for testing	
 	} else {
 		console.log("Recieved Object: \n"+JSON.stringify(tree));  
-  	process.exit(code=0)	
 	}
 })
+
+
+ldap2json.getSecurityGroups(getTreeOptions,function(e,groups){
+	if (e){
+		console.log("Error : : : : "+e);
+	} else {
+		groups.forEach(function(grp){
+			console.log(grp.name);
+		});	
+	}
+});
+
