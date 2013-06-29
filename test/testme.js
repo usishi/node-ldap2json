c4c6105d48c0f93b69a0e5e7476c7b926cc45eb6
Fatih Soydan <fatihsoydan@fatihsoydan.com>
2013-06-29 14:40:56 +0300

    CheckUser added, vows used

--- test/testme.js
+++ test/testme.js
@@ -1,45 +1,42 @@
 var 
 	ldap2json = require('../lib/ldap2json')
 	,vows 		= require('vows')
+	,assert   = require('assert')
 	;
 
 
 var getTreeOptions = {
   host : '10.1.60.5',
-  username : 'bh\\Administrator',
-  password : 'Genel1234',
+  adminuser : 'bh\\Administrator',
+  adminpass : 'Genel1234',
   base : 'dc=bh,dc=pvt' 
 }
 
 
-/// FIND A PUBLIC LDAP Server for testing	
 
-
-ldap2json.checkUser(getTreeOptions,function(e){
-	if (e){
-		console.log("Error : : : : "+e);
-	} else {
-		console.log("Authentication OK");  
-	}
-});
-
-
-ldap2json.getJson(getTreeOptions,function(e,tree){
-	if (e){
-		console.log("Error : : : : "+e);
-	} else {
-		console.log("Recieved Object: \n"+JSON.stringify(tree));  
-	}
-})
-
-
-ldap2json.getSecurityGroups(getTreeOptions,function(e,groups){
-	if (e){
-		console.log("Error : : : : "+e);
-	} else {
-		groups.forEach(function(grp){
-			console.log(grp.name);
-		});	
-	}
-});
-
+vows.describe('ldap2json').addBatch({   
+    'CheckUser': {
+      topic : function(){
+      	ldap2json.checkUser(getTreeOptions,'serkan','Genel123',this.callback);
+      },
+      'callback':function(e,usr){
+      	assert.ifError(e);
+			}
+    },
+    'Get Domain Tree' : {
+    	topic : function() {
+				ldap2json.getJson(getTreeOptions,this.callback);    		
+    	},
+    	'callback':function(e,tree){
+    		assert.ifError(e);
+    	}
+    },
+    'Get Security Groups' : {
+    	topic : function() {
+				ldap2json.getSecurityGroups(getTreeOptions,this.callback);    		
+    	},
+    	'callback':function(e,groups){
+    		assert.ifError(e);
+    	}
+    }
+}).export(module);
