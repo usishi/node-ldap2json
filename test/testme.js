ad184e97b9dfcc545b144962c79ea4052d673428
Fatih Soydan <fatihsoydan@fatihsoydan.com>
2013-08-15 14:03:45 +0300

    test framework changed  and some bugfixes

--- test/testme.js
+++ test/testme.js
@@ -2,45 +2,57 @@ var
 	ldap2json = require('../lib/ldap2json')
 	,vows 		= require('vows')
 	,assert   = require('assert')
+  ,should  = require('should')
 	;
 
 
-var getTreeOptions = {
-  host : '10.1.60.5',
-  adminuser : 'bh\\Administrator',
-  adminpass : 'Genel1234',
-  base : 'dc=bh,dc=pvt' 
+var testVariables = {
+
+  TreeOptions : {
+    host : '10.1.60.5',
+    adminuser : 'bh\\Administrator',
+    adminpass : '1',
+    base : 'dc=bh,dc=pvt' 
+  },
+  testuser:'serkan',
+  testuserpass : '1',
+  searchkeyword : 'gök'  
 }
 
 
 
-vows.describe('ldap2json').addBatch({   
-    'CheckUser': {
-      topic : function(){
-      	ldap2json.checkUser(getTreeOptions,'serkan','Genel123',this.callback);
-      },
-      'callback':function(e,usr){
-      	assert.ifError(e);
-        console.log('OK');
-        console.log(usr);
-			}
-    },
-    'Get Domain Tree' : {
-    	topic : function() {
-				ldap2json.getJson(getTreeOptions,this.callback);    		
-    	},
-    	'callback':function(e,tree){
-    		assert.ifError(e);
-        console.log(tree);
-    	}
-    },
-    'Get Security Groups' : {
-    	topic : function() {
-				ldap2json.getSecurityGroups(getTreeOptions,this.callback);    		
-    	},
-    	'callback':function(e,groups){
-    		assert.ifError(e);
-        console.log(groups);
-    	}
-    }
-}).export(module);
+describe('Ldap2Json',function(){
+  describe('CheckUser',function(){
+    it('returns a user',function(done){
+      ldap2json.checkUser(testVariables.TreeOptions,'serkan','1',function(e,usr){
+        usr.should.be.object;
+        done();
+      });
+    });
+  });
+  describe('Get Domain Tree',function(){
+    it('returns an array',function(done){
+      ldap2json.getJson(testVariables.TreeOptions,function(e,tree){
+        tree.should.be.object;
+        done();
+      });
+    });
+  });
+  describe('Get Security Groups',function(){
+    it('returns an array',function(done){
+      ldap2json.getSecurityGroups(testVariables.TreeOptions,function(e,grps){
+        grps.should.be.object;
+        done();
+      });
+    });
+  });
+  describe('Search Users',function(){
+    it('returns an array',function(done){
+      ldap2json.searchUser(testVariables.TreeOptions,testVariables.searchkeyword,function(e,items){
+        items.should.be.object;
+        done();
+      });
+    });
+  });
+}); 
+
